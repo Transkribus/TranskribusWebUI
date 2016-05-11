1838c7b645bdb831e9b7b410b32fc47e85850a7a
Rory McNicholl <cziaarm@localhost.localdomain>
2016-05-11 09:58:47 +0100

    tidying

--- library/services.py
+++ library/services.py
@@ -10,14 +10,14 @@ s = requests.Session()
 
 def t_login(user, pw):
     url = settings.TRP_URL+'auth/login' 
-    sys.stdout.write("trp URL: %s%%   \r\n" % (url) )
+    sys.stdout.write("### IN t_login: %s%%   \r\n" % (url) )
     sys.stdout.flush()
 
     params = {'user': user, 'pw': pw}
     headers = {'content-type': 'application/x-www-form-urlencoded'}
     #for no connection 
     if settings.OFFLINE:
-	if settings.ADMIN_PASSWORD != pw:
+	if settings.ADMIN_PASSWORD == pw:
 		user_xml = settings.TEST_USER_XML
 	else:
 		return None
@@ -30,14 +30,35 @@ def t_login(user, pw):
 
     t_user=xmltodict.parse(user_xml)
 
-    sys.stdout.write("trpUserLogin: %s%%   \r\n" % (t_user['trpUserLogin']) )
-    sys.stdout.flush()
+#    sys.stdout.write("trpUserLogin: %s%%   \r\n" % (t_user['trpUserLogin']) )
+#    sys.stdout.flush()
 
     return t_user['trpUserLogin']
+
+#refresh transkribus session (called by t_login_required decorator to test persistence/validity of transkribus session)
+def t_refresh():
+    url = settings.TRP_URL+'auth/refresh' 
+    sys.stdout.write("### IN t_refresh: %s%%   \r\n" % (url) )
+    sys.stdout.flush()
+
+
+    r = s.post(url, verify=False)
+
+    sys.stdout.write("STATUS CODE: %s%%   \r\n" % (r.status_code) )
+    sys.stdout.flush()
+
+    if r.status_code != requests.codes.ok:
+        return None
+    else:
+	return True
 
 def t_collections():
 
     url = settings.TRP_URL+'collections/list'
+
+    sys.stdout.write("### IN t_collectionssss: %s%%   \r\n" % (url) )
+    sys.stdout.flush()
+
     headers = {'content-type': 'application/json'}
     params = {} 
     #for no connection:
@@ -54,8 +75,8 @@ def t_collections():
 
     collections = json.loads(collections_json) 
 
-    sys.stdout.write("Collections data: %s%%   \r\n" % (collections) )
-    sys.stdout.flush()
+#    sys.stdout.write("Collections data: %s%%   \r\n" % (collections) )
+#    sys.stdout.flush()
 #    sys.stdout.write("User: %s%%   \r\n" % (request.user) )
 #    sys.stdout.flush()
 
@@ -64,6 +85,9 @@ def t_collections():
 def t_collection(collId):
 
     url = settings.TRP_URL+'collections/'+collId+'/list'
+    sys.stdout.write("### IN t_collectionnnn: %s%%   \r\n" % (url) )
+    sys.stdout.flush()
+
     headers = {'content-type': 'application/json'}
     params = {} 
     #for no connection:
@@ -83,15 +107,19 @@ def t_collection(collId):
     for doc in collection:
 	doc['key'] = doc['docId']
 	doc['folder'] = 'true'
+	#Infer enough to render a basic thumbnail grid... (will need images eventually)
 	doc['children'] = [{'title': "Page "+str(x+1), 'key': str(doc['key'])+"_"+str(x+1), 'page': str(x+1)} for x in range(doc['nrOfPages'])]
     
-    sys.stdout.write("Collection data: %s%%   \r\n" % (collection) )
-    sys.stdout.flush()
+  #  sys.stdout.write("Collection data: %s%%   \r\n" % (collection) )
+   # sys.stdout.flush()
    # sys.stdout.write("User: %s%%   \r\n" % (request.user) )
    # sys.stdout.flush()
 
     return collection
 
+# Shall we propose a streamlined json version of document metadata for pages where we want more than one doc displayed?
+# or are we happy to proceed with derived page level metadata (see t_collection) regardless we use the big XML (or maybe big json)
+# for 1 doc pages...
 def t_document(collId, docId):
 
     url = settings.TRP_URL+'collections/'+collId+'/'+docId+'/fulldoc.xml'
@@ -99,7 +127,7 @@ def t_document(collId, docId):
     params = {} 
     #for no connection:
     if settings.OFFLINE:
-	doc_xml = settings.TEST_DOCUMENT_XML
+	doc_xml = settings.TEST_FULL_DOC_XML
     #for connection 
     else:
     	r = s.get(url, params=params, verify=False, headers=headers)
@@ -107,19 +135,12 @@ def t_document(collId, docId):
         	return None
     	doc_xml = r.content
 
-    t_doc=xmltodict.parse(doc_xml)
-
-    sys.stdout.write("Document metadata for doc #%s%%: %s%%   \r\n" % (docId, t_doc) )
+    sys.stdout.write("Document metadata XML: #%s%%   \r\n" % (doc_xml) )
     sys.stdout.flush()
 
-#    for page in collection:
-#	doc['key'] = doc['docId']
-#	doc['folder'] = 'true'
-#	doc['children'] = [{'title': "Page "+str(x+1)} for x in range(doc['nrOfPages'])]
-    
-#    sys.stdout.write("Collection data: %s%%   \r\n" % (collection) )
-#    sys.stdout.flush()
-   # sys.stdout.write("User: %s%%   \r\n" % (request.user) )
-   # sys.stdout.flush()
+    t_doc=xmltodict.parse(doc_xml)
 
-    return t_doc
+#    sys.stdout.write("Document metadata for doc #%s%%: %s%%   \r\n" % (docId, t_doc) )
+ #   sys.stdout.flush()
+
+    return t_doc.get('trpDoc')
