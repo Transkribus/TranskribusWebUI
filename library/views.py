15aa37901c6c15153d4f562973d8030f63e74b40
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-07-08 18:42:01 +0100

    stashed references to profiler and started putting teeth into the register view

--- library/views.py
+++ library/views.py
@@ -11,8 +11,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .decorators import t_login_required#, profile
+from django.utils.translation import ugettext_lazy as _
 
+from .decorators import t_login_required
+#from profiler import profile #profile is a decorator, but things get circular if I include it in decorators.py so...
+
+import settings
 import services
 import navigation
 import json
@@ -32,23 +36,35 @@ def register(request):
             return HttpResponseRedirect('/library/')
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
+	sys.stdout.write("### IN t_register \r\n" )
         # create a form instance and populate it with data from the request:
         form = RegisterForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
+	    sys.stdout.write("### IN form is valid \r\n" )
+
             # user = User.objects.create_user(form.cleaned_data['username'],password=form.cleaned_data['password'],email=form.cleaned_data['email'],first_name=form.cleaned_data['given_name'],last_name=form.cleaned_data['family_name'])
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-#TODO        services.t_register(form)
-            return HttpResponseRedirect('/library/profile')
-
+            try: 
+            	services.t_register(request)
+            	return HttpResponseRedirect('/library/profile')
+		#tried out modal here and it is noce (but not really for registration)
+#	        messages.info(request, _('Registration requested please check your email.'))
+#                return HttpResponse(json.dumps({'RESET': 'true', 'MESSAGE': render_to_string('libraryapp/message_modal.html', request=request)}), content_type='text/plain')
+	    except ValueError as err:
+		sys.stdout.write("### t_register response ERROR RAISED: %s  \r\n" % (err) )
+#		return render(request, 'registration/register.html', {'register_form': form} )
+		#Why the f**k won't this redirect?!? TODO fix or try another method
+		return HttpResponseRedirect('/library/error')
+    
     # if a GET (or any other method) we'll create a blank form
     else:
         form = RegisterForm()
 
+    #Recpatch errors are not properly dislpayed by bootstrap-form... hmph
     return render(request, 'registration/register.html', {'register_form': form} )
-
 
 def index(request):
     return render(request, 'libraryapp/homepage.html' )
@@ -580,7 +596,7 @@ def profile(request):
     collections = request.session.get("collections");
     return render(request, 'libraryapp/profile.html', {'collections': collections})
 
-#error pages
+#error pages (where not handled by modals
 def collection_noaccess(request, collId):
     if(request.get_full_path() == request.META.get("HTTP_REFERER") or re.match(r'^.*login.*', request.META.get("HTTP_REFERER"))):
         back = None
@@ -589,6 +605,13 @@ def collection_noaccess(request, collId):
 
     return render(request, 'libraryapp/error.html', {
                 'msg' : _("I'm afraid you are not allowed to access this collection"),
+                'back' : back,
+            })
+def error(request):
+    back = settings.APP_BASEURL+"/register"
+
+    return render(request, 'libraryapp/error.html', {
+                'msg' : messages,
                 'back' : back,
             })
 
