a23a599f709257eb16f6e36a44b977353ad1c249
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-07-08 12:54:22 +0100

    sort out register variables

--- library/forms.py
+++ library/forms.py
@@ -2,13 +2,15 @@ from django import forms
 from django.utils import translation
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from nocaptcha_recaptcha.fields import NoReCaptchaField
 
 class RegisterForm(forms.Form):
-    given_name = forms.CharField(label=_('Given Name'), max_length=100)
-    family_name = forms.CharField(label=_('Family Name'), max_length=100)
-    email = forms.EmailField(label=_('Email'), max_length=100)
-    username = forms.CharField(label=_('Username'), max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput(), label=_('Password'), max_length=100)
+    firstName = forms.CharField(label=_('Given Name'), max_length=100)
+    familyName = forms.CharField(label=_('Family Name'), max_length=100)
+    #email = forms.EmailField(label=_('Email'), max_length=100)
+    user = forms.EmailField(label=_('Username'), max_length=100)
+    pw = forms.CharField(widget=forms.PasswordInput(), label=_('Password'), max_length=100)
+    captcha = NoReCaptchaField()
 
 class IngestMetsUrlForm(forms.Form):
     url = forms.URLField(label='METS XML file URL')
