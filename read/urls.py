2fd3eaf03683527d33879aebc66c716412b439e2
Matti Jokinen <matti@linux-nhtg.suse>
2016-08-05 13:41:49 +0300

    Added review

--- read/urls.py
+++ read/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^library/', include('library.urls')),    
     url(r'^admin/', admin.site.urls),
+    url(r'^review/', include('review.urls')),
 ]
