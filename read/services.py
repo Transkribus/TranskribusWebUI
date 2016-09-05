4bcd3d5755c7b58b52cdb231e9ec0d36d5ade400
be jo hu <bejohu@yandex.com>
2016-09-05 23:18:05 +0530

    fixed type issue in services.py using json.loads

--- read/services.py
+++ read/services.py
@@ -51,7 +51,7 @@ def t_register(request):
         raise ValueError("Transkribus registration error",str(r.status_code),r.content)
         return None
 
-    user_xml = r.content
+    user_xml = r.text
 
     t_user=xmltodict.parse(user_xml)
 
@@ -75,7 +75,7 @@ def t_login(user, pw):
     sys.stdout.flush()
     if r.status_code != requests.codes.ok:
         return None
-    user_xml = r.content
+    user_xml = r.text
 
     t_user=xmltodict.parse(user_xml)
 
@@ -116,7 +116,7 @@ def t_collections():
         sys.stdout.flush()
         return None
 
-    collections_json=r.content.decode()
+    collections_json=r.text
     collections = json.loads(collections_json)
 
     #use common param 'key' for ids
@@ -187,7 +187,7 @@ def t_document(request, collId, docId, nrOfTranscripts=None):
     if r.status_code != requests.codes.ok:
         return None
 
-    doc_json = r.content
+    doc_json = r.text
     t_doc = json.loads(doc_json)
 
     pages = t_doc.get("pageList").get("pages")
@@ -227,7 +227,8 @@ def t_page(request,collId, docId, page, nrOfTranscripts=None):
     if r.status_code != requests.codes.ok:
         return None
 
-    page_json = r.content
+    # page_json = r.content
+    page_json = r.text
     t_page = json.loads(page_json)
 
     #TODO would prefer a pageId rather than "page" which is a the page number
@@ -269,7 +270,7 @@ def t_current_transcript(request,collId, docId, page):
     if r.status_code != requests.codes.ok:
         return None
 
-    transcript_json = r.content
+    transcript_json = r.text
     t_transcript = json.loads(transcript_json)
 
     t_transcript['key'] = t_transcript.get('tsId')
@@ -321,7 +322,7 @@ def t_transcript_xml(request,transcriptId,url):
 
     if r.status_code != requests.codes.ok:
         return None
-    return r.content
+    return r.text
 
 # Saves transcripts. TODO Statuses...
 def t_save_transcript(request, transcript_xml, collId, docId, page):
@@ -418,7 +419,7 @@ def t_jobs(status = ''):
         sys.stdout.write("Error getting jobs: %s \r\n ERROR: %s" % (r.status_code, r.content))
         sys.stdout.flush()
         return None
-    jobs_json=r.content
+    jobs_json=r.text
     jobs = json.loads(jobs_json)
     return jobs
 
@@ -430,7 +431,7 @@ def t_job_count(status = ''):
         sys.stdout.write("Error getting job count: %s \r\n ERROR: %s" % (r.status_code, r.content))
         sys.stdout.flush()
         return None
-    count=r.content
+    count=r.text
     return count
 
 def t_kill_job(job_id):
