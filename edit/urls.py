a8e3d0be7563f3d1d564e2113f5a522e0111c4f5
matti <matti@localhost>
2016-09-13 14:45:08 +0300

    Django2JavaScript like this
    Static JavaScript introduces other issues. Trying out this solution.

--- edit/urls.py
+++ edit/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'correct/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/(\w+)$', views.correct, name='correct'),
     url(r'^correct_modal$', views.correct_modal, name='correct_modal'),
+    url(r'^correct_js$', views.correct_js, name='correct_js'),
 ]
