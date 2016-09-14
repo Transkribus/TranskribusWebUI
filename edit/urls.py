eadab81d36f00194cf06e8e9d6cebfbb0020f5ff
matti <matti@localhost>
2016-09-14 15:23:32 +0300

    Cleanup

--- edit/urls.py
+++ edit/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'correct/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/(\w+)$', views.correct, name='correct'),
-    url(r'^correct_modal$', views.correct_modal, name='correct_modal'),
+    url(r'correct/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='correct'),
     url(r'^correct_js$', views.correct_js, name='correct_js'),
 ]
