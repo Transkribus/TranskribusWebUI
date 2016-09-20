0d1dec75977e7fd6ce2c641dcb28d0d0b8d620b3
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-09-20 13:23:44 +0100

    removed debug

--- library/navigation.py
+++ library/navigation.py
@@ -3,6 +3,7 @@ import requests
 from django.conf import settings
 import sys
 from . import settings
+from read.utils import t_log
 
 hierarchy = {'word': 'line',
              'line': 'region',
@@ -20,17 +21,11 @@ def up_next_prev(this_level,this_id, data,parent_ids=None):
         for id in parent_ids: #TODO work out slicker way to do this
             up.append(id)
 
-#    sys.stdout.write("NAV DATA: %s   \r\n" % (data) )
-#    sys.stdout.flush()
-
     #assumptions are that we want to traverse by id and do that they will be presented to us in order (can sort data if not)
     next_promise=False
     last_id=None
     for x in data:
         that_id = x.get("key")
-#        sys.stdout.write("NAV DATA: %s == %s \r\n" % (that_id,this_id) )
-#        sys.stdout.flush()
-
         if next_promise:
             next=that_id
             next_promise=False
@@ -39,10 +34,6 @@ def up_next_prev(this_level,this_id, data,parent_ids=None):
             next_promise=True
         last_id = that_id
 
-  #  sys.stdout.write("UP: %s \nNEXT: %s\nPREV: %s  \r\n" % (up, next, prev) )
-  #  sys.stdout.flush()
-
-#    return {'up':"/library/"+"/".join(up),
     return {'up': settings.APP_BASEURL+'/'.join(up),
                 'next':next,
                 'prev':prev}
