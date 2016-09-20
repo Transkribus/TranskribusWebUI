4017b65d708c828078c5a1ba6b4837912c2a3c34
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-09-20 13:19:30 +0100

    dashboard templates for lists/tables

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,9 +4,19 @@ from django.shortcuts import render
 #from django.contrib.auth.decorators import login_required
 from read.decorators import t_login_required
 
+from read.services import *
 
 # Create your views here.
 
 @t_login_required
 def index(request):
-    return render(request, 'dashboard/homepage.html' )
+    collections = request.session.get("collections");
+
+    t_actions_info(request)
+    sys.stdout.write("### [VIEW] action_types: %s   \r\n" % (request.session.get('action_types')) )
+
+    #get all(?) actions for logged in user
+    actions=t_list_actions(request)
+    sys.stdout.write("### [VIEW] actions: %s   \r\n" % (request.session.get('actions')) )
+
+    return render(request, 'dashboard/homepage.html', {'collections': collections} )
