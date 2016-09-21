166de7e2ef9257268f03fd5ea70c38c8362eb744
be jo hu <bejohu@yandex.com>
2016-09-22 05:26:46 +0530

    store api session id in user data model

--- read/backends.py
+++ read/backends.py
@@ -45,6 +45,7 @@ class TranskribusBackend(object):
                 tsdata = TSData.objects.create(user=user)
             tsdata.gender=t_user['gender']
             tsdata.affiliation=t_user['affiliation']
+            tsdata.session_id=t_user['sessionId']
 
             ######
             # If we have some local user data that we need to update dependent on whatever TS-REST returns do so here
