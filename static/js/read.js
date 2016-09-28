5b6849ed64b4e52891a2995ccd9f247af397bf9a
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-09-28 08:04:27 +0100

    data table and datepicker/slider input handling for actions table in dashboard

--- static/js/read.js
+++ static/js/read.js
@@ -10,15 +10,97 @@ $(document).ready(function(){
 		$(".read_nav_in").show();
 		$(".read_nav_out").hide();
 	}
-
 });
+
 
 $(document).ready(function(){
 	$("#id_language").on("change keyup", function () {
 		$(this).closest("form").submit();
 	});
+	actions_table = init_actions_table();
+	init_date_inputs(actions_table);
 });
 
+function init_date_inputs(actions_table){
+
+    var min = new Date('2016.01.01').getTime() / 1000;
+    var max = new Date().getTime() / 1000;
+
+    var slider = $( "#slider-range" ).slider({
+      range: true,
+      min: min, //seconds
+      max: max,
+      step: 86400,
+      values: [ min, max ],
+      slide: function( event, ui ) {
+        $( "#start_date" ).val( (new Date(ui.values[ 0 ] *1000).toDateString() ) );
+        $( "#end_date" ).val( (new Date(ui.values[ 1 ] *1000)).toDateString() );
+      },
+      change: function(event, ui){
+	//if the slider values change we reload the data table...
+	actions_table.ajax.reload();
+      }
+    });
+    $( "#start_date" ).datepicker({"dateFormat":"D M dd yy", "onSelect": function(date){
+		console.log("start date picker onSelect");
+		slide_end = slider.slider("option", "values")[1];
+		slider.slider( "option", "values", [($(this).datepicker("getDate")/1000),slide_end] );
+	}}).val(new Date(min*1000).toDateString());
+    $( "#end_date" ).datepicker({"dateFormat":"D M dd yy","onSelect": function(date){
+		console.log("end date picker onSelect");
+		slide_start = slider.slider("option", "values")[0];
+		slider.slider( "option", "values", [slide_start,($(this).datepicker("getDate")/1000)] );
+	}}).val(new Date(max*1000).toDateString());
+
+}
+function init_actions_table(collId,docId){
+	var actions_table = $("#actions_table").DataTable({
+		"processing": true,
+        	"serverSide": true,
+		"ajax": {
+			"url": "./actions_ajax",
+			"data": function ( d ) {
+				console.log("(RE)LOADING",$("#slider-range").data("uiSlider") );
+				if($("#slider-range").data("uiSlider")){
+					console.log("GOGOGO" );
+					return $.extend( {}, d, { 
+						"start_date": ($("#slider-range").slider("option", "values")[0]*1000),
+						"end_date":($("#slider-range").slider("option", "values")[1]*1000) ,
+					});
+				}
+			},
+			"error": function (xhr, error, thrown) {
+				//for now we assume that a problem with the ajax request means 
+				//that TS REST session is expired and you need logged out... 
+				//this could be annoying if not the case though!
+				//TODO needs query string too...?
+				alert("There was an issue communicating with the transkribus service Please try again, if the problem persists send the error below to....\n "+error);
+//				window.location.replace("/login/?next="+window.location.pathname)
+			}
+		},		
+		"sDom": "rltip",
+		"length" : 5,
+		"lengthMenu": [ 5, 10, 20, 50, 100 ],
+		//ordering should be handled server side
+//		"order": [[ 0, "desc" ]],
+		"ordering": false, 
+		"columns": [
+		    { "data": "time" },
+		    { "data": "colId" },
+		    { "data": "docId" },
+		    { "data": "pageId" },
+		    { "data": "userName" },
+		    { "data": "type" }
+        	],
+	});
+	$(".table_filter").on("click", function(){
+		 actions_table.columns( 5 )
+        	.search( this.value )
+        	.draw();
+		return false;
+	});
+	return actions_table;
+}
 /* Collections */
 
 glyph_opts = {
@@ -107,7 +189,7 @@ $(document).ready(function(){
 		var button = this;
 //		$(this).parents(".expandable").removeClass("col-md-4", function(){
 	
-		var class_to_remove = ($(this).parents(".expandable").attr("class").match(/(^|\s)col-md-\S+/g) || []).join(' ');
+		var class_to_remove = ($(this).parents(".expandable").attr("class").match(/(^|\s)col-md-\S+/g) || []).join(" ");
 		$(this).parents(".expandable").removeClass(class_to_remove, function(){
 
 			$(this).addClass("col-md-12");
