4b1bdadd88523d78d556b9a7ee3c75c7df7a192e
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-10-05 17:18:00 +0100

    avoid viewname clash

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^([0-9]+)$', views.collection, name='collection'),
-    url(r'^([0-9]+)/([0-9]+)$', views.document, name='document'),
+    url(r'^([0-9]+)$', views.d_collection, name='d_collection'),
+    url(r'^([0-9]+)/([0-9]+)$', views.d_document, name='d_document'),
     url(r'^actions_for_table_ajax$', views.actions_for_table_ajax, name='actions_for_table_ajax'),
     url(r'^actions_for_table_ajax/([0-9]+)$', views.actions_for_table_ajax, name='actions_for_table_ajax'),
+    url(r'^collections_for_table_ajax$', views.collections_for_table_ajax, name='collections_for_table_ajax'),
     url(r'^actions_for_chart_ajax$', views.actions_for_chart_ajax, name='actions_for_chart_ajax'),
     url(r'^$', views.index, name='index'),
 ]
