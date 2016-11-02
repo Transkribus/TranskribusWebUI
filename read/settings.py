4fe9ea072a077b52b0d3652307c21ca2f51bcf8d
kansallisarkisto <matti.jokinen@narc.fi>
2016-11-02 13:27:22 +0200

    collectstatic from now on

--- read/settings.py
+++ read/settings.py
@@ -162,10 +162,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = 'static'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+
 ]
 
 #TODO for when use is heavier use memcached
