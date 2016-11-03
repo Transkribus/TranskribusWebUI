c5185d09c167ae8644082504d8c6166856a6300d
matti <matti@localhost>
2016-11-03 12:55:46 +0200

    Proofread view develompent begun

--- edit/urls.py
+++ edit/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'correct/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='correct'),
+    url(r'proofread/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.proofread, name='proofread'),
 ]
