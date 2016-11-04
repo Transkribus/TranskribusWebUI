b93053596be67b2387333234473d4e9faf55c157
matti <matti@localhost>
2016-11-04 10:49:26 +0200

    Quick and dirty copy paste to get started with the implementation

--- edit/views.py
+++ edit/views.py
@@ -32,6 +32,58 @@ from library.forms import RegisterForm, IngestMetsUrlForm, MetsFileForm
 #from profiler import profile #profile is a decorator, but things get circular if I include it in decorators.py so...
 
 @t_login_required
+def proofread(request, collId, docId, page, transcriptId):# TODO Decide whether to select which transcript to work with unless it should always be the newest?
+    current_transcript = t_current_transcript(request, collId, docId, page)
+    transcript = t_transcript(request, current_transcript.get("tsId"), current_transcript.get("url"))
+    transcriptId = str(transcript.get("tsId"))
+    if request.method == 'POST':# This is by JQuery...
+        content = json.loads(request.POST.get('content'))
+        transcript_xml = t_transcript_xml(request, transcriptId, current_transcript.get("url"))
+        transcript_root = ElementTree.fromstring(transcript_xml)
+        # TODO Decide what to do about regionId... It's not necessary....
+        for text_region in transcript_root.iter('{http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15}TextRegion'):# We have to have the namespace...
+            regionTextEquiv = ""
+            for line in text_region.iter('{http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15}TextLine'):
+                 modified_text = content.get(id = line.get("id"))
+                 regionTextEquiv += modified_text +"\r\n"
+                 line.find('{http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15}TextEquiv').find('{http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15}Unicode').text = modified_text
+            text_region.find('{http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15}TextEquiv').find('{http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15}Unicode').text = regionTextEquiv
+        t_save_transcript(request, ElementTree.tostring(transcript_root), collId, docId, page)
+        current_transcript = t_current_transcript(request, collId, docId, page)# We want the updated transcript now.
+        success_message = str(_("Transcript saved!"))
+        return HttpResponse("<div class='alert alert-success'>" + success_message + "</div>", content_type="text/plain")
+    else:
+        regions=transcript.get("PcGts").get("Page").get("TextRegion");
+        
+        if isinstance(regions, dict):
+            regions = [regions]
+
+        lineList = []
+        if regions:
+            for x in regions:
+                lines = x.get("TextLine")
+                if isinstance(lines, dict):
+                    lineList.extend([lines])
+                else: # Assume that lines is a list of lines
+                    for line in lines:
+                        lineList.extend([line])
+        
+        content_dict = {}
+        # TODO Use "readingorder"?
+        if lineList:
+            for line in lineList:
+                line_crop = crop(line.get("Coords").get("@points"))#,True)
+                line['crop'] = line_crop
+                line_id = line.get("@id")
+                line['id'] = line_id
+                line['Unicode'] = line.get('TextEquiv').get('Unicode')
+        
+    return render(request, 'edit/proofread.html', {
+        'imageUrl': t_document(request, collId, docId, -1).get('pageList').get('pages')[int(page) - 1].get("url"),
+        'lines': lineList
+        })
+
+@t_login_required
 def correct(request, collId, docId, page, transcriptId):# TODO Decide whether to select which transcript to work with unless it should always be the newest?
     current_transcript = t_current_transcript(request, collId, docId, page)
     transcript = t_transcript(request, current_transcript.get("tsId"), current_transcript.get("url"))
