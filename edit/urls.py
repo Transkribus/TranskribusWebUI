348484733cefa20c6d4713c9d4eceeb0d29bfa53
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-11-09 15:38:33 +0000

    edit url for 3 OR 4 ids, *may* allow transcript selection

--- edit/urls.py
+++ edit/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'proofread/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.proofread, name='proofread'),
     #allow edit of current transcript with no transcriptId
     url(r'correct/([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='correct'),
+    url(r'correct/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='correct'),
+
 ]
