a1721db68f48af6ee20d69c21c2a92bc977059bc
matti <matti@localhost>
2016-11-10 08:00:00 +0200

    URL to transcribe added

--- edit/urls.py
+++ edit/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'proofread/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.proofread, name='proofread'),
     #allow edit of current transcript with no transcriptId
     url(r'correct/([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='correct'),
+    url(r'transcribe/([0-9]+)/([0-9]+)/([0-9]+)$', views.transcribe, name='transcribe'),
 ]
