11eb204fb04b345858ea84510256eb78f07b6ba8
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-11-12 01:47:39 +0000

    local_settings include and new captcha params

--- read/settings.py
+++ read/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-#    'nocaptcha_recaptcha',
+    'captcha',
     'read',
     'library',
     'review',
@@ -160,6 +160,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+NOCAPTCHA = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
@@ -214,6 +216,6 @@ TRP_URL = 'https://transkribus.eu/TrpServer/rest/'
 PROFILE_LOG_BASE = '/tmp/'
 
 try:
-    from local_settings import *
+    from read.local_settings import *
 except ImportError:
     pass
